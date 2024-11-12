a4ef20967abc81c587a8e19b64c4de7f0fee612a
gargi-github <gargi.chowdhury@gmail.com>
2024-11-12 20:42:47 +0530

    upload responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
 
-      // key & click
+    // key & click
     /*window.oncontextmenu = function(){return false;}
     document.onkeydown = function(e) {
         if (e.ctrlKey &&
@@ -17,86 +17,86 @@ jQuery(document).ready(function() {
     };*/
 
 
-    jQuery(window).on('scroll', function() {
-		var scroll = jQuery(window).scrollTop();
-		if (scroll >= 50) {
-			jQuery('.header-wrapper').addClass('fixed');
-		} else {
-			jQuery('.header-wrapper').removeClass('fixed');
-		}
-	});
+    jQuery(window).on('scroll', function () {
+        var scroll = jQuery(window).scrollTop();
+        if (scroll >= 50) {
+            jQuery('.header-wrapper').addClass('fixed');
+        } else {
+            jQuery('.header-wrapper').removeClass('fixed');
+        }
+    });
 
 
-    jQuery(window).scroll(function(){
+    jQuery(window).scroll(function () {
         if (jQuery(this).scrollTop() > 100) {
             jQuery('.scroll-top').fadeIn();
         } else {
             jQuery('.scroll-top').fadeOut();
         }
     });
-    jQuery('.scroll-top').click(function(){
-        jQuery('html, body').animate({scrollTop : 0},800);
+    jQuery('.scroll-top').click(function () {
+        jQuery('html, body').animate({ scrollTop: 0 }, 800);
         return false;
     });
 
 
-    var xz=0;
-    jQuery(".open-menu, .close-menu").click(function(){
-        if(xz==0){
-            jQuery(".navigation").animate({"left":0},300);
+    var xz = 0;
+    jQuery(".open-menu, .close-menu").click(function () {
+        if (xz == 0) {
+            jQuery(".navigation").animate({ "left": 0 }, 300);
             xz++;
-        }else{
-            jQuery(".navigation").animate({"left":"-100%"},300);
+        } else {
+            jQuery(".navigation").animate({ "left": "-100%" }, 300);
             xz--;
         }
         return false;
     });
     jQuery(document).on('click', function (event) {
         if (!jQuery(event.target).closest('.navigation').length) {
-            jQuery(".navigation").animate({"left":"-100%"},300);
+            jQuery(".navigation").animate({ "left": "-100%" }, 300);
             xz = 0;
         }
     });
 
 
     jQuery('.banner .owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        items:1,
-        dots:false,
-        nav:true,
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
         navText: [
-			'<i class="fa-solid fa-chevron-left"></i>',
-			'<i class="fa-solid fa-chevron-right"></i>'
-		],
-        autoplay:true,
-        autoplayTimeout:4000,
-        autoplayHoverPause:true
+            '<i class="fa-solid fa-chevron-left"></i>',
+            '<i class="fa-solid fa-chevron-right"></i>'
+        ],
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true
     });
 
 
     jQuery('.client-wrap .owl-carousel').owlCarousel({
-        loop:true,
-        margin:30,
-        autoplay:true,
-        autoplayTimeout:1500,
-        autoplayHoverPause:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 1500,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            479:{
-                items:2
+            479: {
+                items: 2
             },
-            767:{
-                items:3
+            767: {
+                items: 3
             },
-            991:{
-                items:4
+            992: {
+                items: 4
             },
-            1199:{
-                items:5
+            1199: {
+                items: 5
             }
         }
     });
@@ -105,8 +105,8 @@ jQuery(document).ready(function() {
     AOS.init();
 
     jQuery('[data-fancybox="gallery"]').fancybox({
-        'titlePosition':'inside',
-	    'titleFormat':formatTitle
+        'titlePosition': 'inside',
+        'titleFormat': formatTitle
     });
 
 
